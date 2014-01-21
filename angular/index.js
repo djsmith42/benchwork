f5fcfad7892b418a57e111f2abef7b31df30330f
Dave Smith <dave@thesmithfam.org>
2014-01-21 12:25:39 -0500

    Use $$postDigest instead of our dirty hack for measuing Angular performance

--- angular/index.js
+++ angular/index.js
@@ -1,39 +1,23 @@
 (function() {
 var app = angular.module("BenchworkApp", ["BenchworkServices"])
 
-app.controller("MainController", function($scope, $log, bwTimeIt) {
+app.controller("MainController", function($scope, $log, bwNow) {
   $scope.test1 = function() {
     $log.info("Starting test 1");
-    bwTimeIt(function() {
-      $scope.test1things = [];
-      for (var i=0; i<1000; i++) {
-        $scope.test1things.push({a: i});
-      }
-    }).then(function(ms) {
-      $scope.test1results = {ms: ms};
+    $scope.$$postDigest(function() {
+        var end = bwNow();
+        $scope.test1results = {ms: end-start}
+        $scope.$apply();
     });
+    var start = bwNow();
+    $scope.test1things = [];
+    for (var i=0; i<1000; i++) {
+      $scope.test1things.push({a: i});
+    }
   }
 });
 
 angular.module("BenchworkServices", []).
-factory('bwTimeIt', function($rootScope, $q, bwNow) {
-  return function(fn) {
-     var deferred = $q.defer();
-     setTimeout(function() {
-       $rootScope.$digest();
-       setTimeout(function() { // get out of any digest in progress
-         fn();
-         var start = bwNow();
-         $rootScope.$digest()
-         var end = bwNow();
-         deferred.resolve(end-start);
-         $rootScope.$digest(); // so our promise gets delivered
-       });
-     });
-
-     return deferred.promise;
-   }
-}).
 factory('bwNow', function($window) {
   return function() {
     if ($window.performance) {
